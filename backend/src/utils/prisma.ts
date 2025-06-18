fd77078fcc36d5d3e0691f9607212cd841c6c5e0
nathanSeixeiro <nathan.seixeiro@gmail.com>
2025-06-17 21:51:30 -0300

    fix: remover opções desnecessárias na configuração do PrismaClient

--- backend/src/utils/prisma.ts
+++ backend/src/utils/prisma.ts
@@ -1,10 +1,5 @@
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient({
-  // Add the missing enableTracing option
-  log: ['query', 'info', 'warn', 'error'],
-  errorFormat: 'pretty',
-  // enableTracing: true // This option does not exist in PrismaClientOptions
-})
-
-export default prisma
+export const prisma = new PrismaClient({
+  log: ["query", "info", "warn", "error"],
+});
