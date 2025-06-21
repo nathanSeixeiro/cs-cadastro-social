0544bffc0b32ffae104456990fc0b17c0c123790
Nathan Seixeiro <108758693+nathanSeixeiro@users.noreply.github.com>
2025-06-20 21:30:13 -0300

    Update server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -5,7 +5,7 @@ import App from "./app";
 import { configDotenv } from "dotenv";
 
 configDotenv();
-const port = parseInt(process.env.PORT || "3001");
+const port = parseInt(process.env.PORT || "3000");
 const server = new App()
   .Start(port)
   .then((port) => console.log(`Server running on port ${port}`))
